9251507dcb489fd165daf37f183c55ed55e4b855
anamsaba18 <anam.saba378@gmail.com>
2024-06-21 14:17:31 -0700

    Get all categories-list component changes done.

--- codepulse.ui/src/app/features/category/add-category/add-category.component.ts
+++ codepulse.ui/src/app/features/category/add-category/add-category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Subscription } from 'rxjs';
 import { CategoryService } from '../services/category.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -15,7 +16,8 @@ export class AddCategoryComponent implements OnDestroy{
 
   private addCategorySubscription? : Subscription;
 
-  constructor(private categoryService: CategoryService) {
+  constructor(private categoryService: CategoryService,
+    private router: Router) {
     this.model = {
       name: '',
       urlHandle: ''
@@ -25,7 +27,7 @@ export class AddCategoryComponent implements OnDestroy{
     this.addCategorySubscription =  this.categoryService.addCategory(this.model)
     .subscribe({
       next:(response) => {
-        console.log('This was successful!');
+        this.router.navigateByUrl('/admin/categories');
       }
     })
 
