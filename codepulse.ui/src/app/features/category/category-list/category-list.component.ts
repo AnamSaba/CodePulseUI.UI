95ba28109c8e32d0cb7c1e76664bde1bdb58bc57
anamsaba18 <anam.saba378@gmail.com>
2024-06-30 12:44:54 -0700

    Filtering sorting and pagination functionality implemented.

--- codepulse.ui/src/app/features/category/category-list/category-list.component.ts
+++ codepulse.ui/src/app/features/category/category-list/category-list.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { CategoryService } from '../services/category.service';
 import { Category } from '../models/category.model';
 
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -12,13 +13,89 @@ export class CategoryListComponent implements OnInit {
 
 
   categories$?: Observable<Category[]>
+  totalCount?: number;
+  list: number[] = [];
+  pageNumber = 1;
+  pageSize = 2;
+
 
   constructor(private categoryService: CategoryService) {
   }
 
 
   ngOnInit(): void {
-    this.categories$ = this.categoryService.getAllCategory()
+
+    this.categoryService.getCategoryCount()
+      .subscribe({
+          next: (value) => {
+            this.totalCount = value;
+
+            this.list = new Array(Math.ceil(value/ this.pageSize))
+
+            this.categories$ = this.categoryService.getAllCategory(
+              undefined,
+              undefined,
+              undefined,
+              this.pageNumber,
+              this.pageSize
+            );
+          }
+      })
+  }
+
+  onSearch(query: string): void {
+    this.categories$ = this.categoryService.getAllCategory(query);
+  }
+
+  sort(sortBy: string, sortDirection: string): void {
+    this.categories$ = this.categoryService.getAllCategory(undefined, sortBy, sortDirection, this.pageNumber, this.pageSize);
+  }
+
+  getPage(pageNumber: number) : void {
+
+      this.pageNumber = pageNumber;
+
+      this.categories$ = this.categoryService.getAllCategory(
+      undefined,
+      undefined,
+      undefined,
+      this.pageNumber,
+      this.pageSize
+    );
+  }
+
+  getPrevPage() : void {
+    if(this.pageNumber - 1 < 1) {
+        return;
+      }
+
+      this.pageNumber -= 1;
+
+      this.categories$ = this.categoryService.getAllCategory(
+        undefined,
+        undefined,
+        undefined,
+        this.pageNumber,
+        this.pageSize
+      );
+
+  }
+
+  getNextPage() : void {
+    if(this.pageNumber + 1 > this.list.length) {
+      return;
+    }
+
+      this.pageNumber += 1;
+
+      this.categories$ = this.categoryService.getAllCategory(
+        undefined,
+        undefined,
+        undefined,
+        this.pageNumber,
+        this.pageSize
+      );
+
   }
 
 }
--- codepulse.ui/src/app/features/category/services/category.service.ts
+++ codepulse.ui/src/app/features/category/services/category.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
@@ -22,8 +22,34 @@ export class CategoryService {
     return this.http.post<void>(`${environment.apiBaseUrl}/api/categories?addAuth=true`, model);
   }
 
-  getAllCategory() : Observable<Category[]>{
-    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
+  getAllCategory(query?: string, sortBy?: string, sortDirection?: string,
+     pageNumber? : number, pageSize?: number) : Observable<Category[]>{
+
+    let params = new HttpParams();
+
+    if(query) {
+      params = params.set('query', query);
+    }
+
+    if(sortBy) {
+      params = params.set('sortBy', sortBy);
+    }
+
+    if(sortDirection) {
+      params = params.set('sortDirection', sortDirection);
+    }
+
+    if(pageNumber) {
+      params = params.set('pageNumber', pageNumber);
+    }
+
+    if(pageSize) {
+      params = params.set('pageSize', pageSize);
+    }
+
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`, {
+      params: params
+    });
   }
 
   getCategoryById(id: string) : Observable<Category>{
@@ -36,6 +62,12 @@ export class CategoryService {
 
   deleteCategory(id: string) : Observable<Category>{
     return this.http.delete<Category>(`${environment.apiBaseUrl}/api/categories/${id}?addAuth=true`);
+  }
+
+  
+
+  getCategoryCount() : Observable<number>{
+    return this.http.get<number>(`${environment.apiBaseUrl}/api/categories/count?addAuth=true`);
   }
 
 }
